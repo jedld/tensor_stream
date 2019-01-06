4abe33f88aecc9861a3146f3fa9ba7c2b93202df
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2019-01-07 03:03:10 +0800

    Add big model deserialization test

--- spec/support/freezer_spec.rb
+++ spec/support/freezer_spec.rb
@@ -11,6 +11,13 @@ RSpec.shared_examples "freezer ops" do
     File.delete(output_file_location) if File.exist?(output_file_location)
   end
 
+  before(:each) do
+    TensorStream::Tensor.reset_counters
+    TensorStream::Operation.reset_counters
+    tf.reset_default_graph
+    sess.clear_session_cache
+  end
+
   specify "convert a checkpoint to a freezed model" do
     checkpoint_file = File.join('spec', 'fixtures', 'lg_model')
 
@@ -24,5 +31,21 @@ RSpec.shared_examples "freezer ops" do
     X = target_graph['Placeholder']
     pred = target_graph['add']
     expect(tr(sess.run(pred, feed_dict: { X => 0.2 }))).to eq(0.8514)
+  end
+
+  specify "load a freezed model and eval it" do
+    target_graph = TensorStream::YamlLoader.new.load_from_file(File.join('spec','fixtures','mnist.yaml'))
+    # Load test images
+    decoded_image_1 = TensorStream.image.decode_png(File.read(File.join('spec', 'fixtures', '0_image.png')), channels: 1)
+    decoded_image_2 = TensorStream.image.decode_png(File.read(File.join('spec', 'fixtures', '1_image.png')), channels: 1)
+
+
+    input = target_graph['Placeholder']
+    output = TensorStream.argmax(target_graph['out'], 1)
+
+    reshaped_images = 255.0.t - [decoded_image_1, decoded_image_2].t.cast(:float32)
+
+    result = sess.run(output, feed_dict: { input => reshaped_images})
+    expect(result).to eq([7, 2])
   end
 end
