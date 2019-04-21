1c9942eb7974309068fd7695ee3efbd8b11b23b6
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2019-04-21 08:45:32 +0800

    Add ops

--- lib/tensor_stream/evaluator/ruby/control_flow_ops.rb
+++ lib/tensor_stream/evaluator/ruby/control_flow_ops.rb
@@ -4,6 +4,21 @@ module TensorStream
       klass.class_eval do
         register_op :enter do |context, tensor, inputs|
         end
+
+        register_op :merge do |context, tensor, inputs|
+        end
+
+        register_op :loop_cond do |context, tensor, inputs|
+        end
+
+        register_op :switch do |context, tensor, inputs|
+        end
+
+        register_op :next_iteration do |context, tensor, inputs|
+        end
+
+        register_op :exit do |context, tensor, inputs|
+        end
       end
     end
   end
